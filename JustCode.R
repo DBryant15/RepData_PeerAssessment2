e89f513710fd9792a09f2a3743fa9ea8d30dfc37
DerekBryant <dwbryan@nppd.com>
2018-04-30 16:59:30 -0500

    20180430 1657 DWB Added Property DMG exporation methods. Have some data dicing in to figure out the unique values in PROPDMGEXP and CROPDMGEXP.

--- JustCode.R
+++ JustCode.R
@@ -13,11 +13,40 @@ if(!file.exists(destfile))
 #read main data into a dataframe
 StormData <- read.csv("StormData.csv", header = TRUE, stringsAsFactors = FALSE)
 
-#TODO: Delete exploratory l8r
-#start exploring data
+#Get data on the injuries and fatalities happening 
+GetIntialPplDMG <- sqldf::sqldf("
+SELECT STATE, 
+EVTYPE, 
+SUM(FATALITIES) as TotalStateFatailies, 
+SUM(INJURIES) as TotalStateInjuries, 
+(SUM(FATALITIES) + SUM(INJURIES)) AS FatalAndInjuresCombined
+FROM StormData
+GROUP BY STATE, EVTYPE
+ORDER BY STATE, EVTYPE")
 
-ex1_GetIntialPplDMG <- sqldf::sqldf("SELECT STATE, EVTYPE, 
-SUM(FATALITIES), SUM(INJURIES), (SUM(FATALITIES) + SUM(INJURIES)) AS FatalAndInjuresCombined
-                                    FROM StormData
-                                    GROUP BY STATE, EVTYPE
-                                    ORDER BY STATE, EVTYPE")
+PplDMG_GroupedBy_EVTYPE <-  sqldf::sqldf("SELECT EVTYPE, SUM(FatalAndInjuresCombined)  
+                                         FROM GetIntialPplDMG
+                                         GROUP BY EVTYPE
+                                         ORDER BY SUM(FatalAndInjuresCombined) DESC")
+
+#PROPDMGEXP
+PROPDMGEXP_uniqueElements <- sqldf::sqldf("SELECT PROPDMGEXP, COUNT(PROPDMGEXP) 
+                                          FROM StormData
+                                          GROUP BY PROPDMGEXP")
+
+#find unique values of CROPDMGEXP
+CROPDMGEXP_uniqueElements <- sqldf::sqldf("SELECT CROPDMGEXP, COUNT(PROPDMGEXP) 
+                                          FROM StormData
+                                          GROUP BY CROPDMGEXP")
+
+
+#Get data on economic effect
+# GetIntialPplDMG <- sqldf::sqldf("
+# SELECT STATE, 
+# EVTYPE, 
+# , 
+# SUM(INJURIES) as TotalStateInjuries, 
+# (SUM(FATALITIES) + SUM(INJURIES)) AS FatalAndInjuresCombined
+# FROM StormData
+# GROUP BY STATE, EVTYPE
+# ORDER BY STATE, EVTYPE")
