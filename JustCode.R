c022c3a7278b35385a1a1fe766a2e312c864988c
DerekBryant <dwbryan@nppd.com>
2018-05-02 13:28:20 -0500

    20180502 1327 DWB I tried to look to remarks (as indicated in the document explaining the data) for the strange EXp values... Maybe look into what the effect of dropping the really strange ones.

--- JustCode.R
+++ JustCode.R
@@ -1,7 +1,7 @@
-#load libraries
+#load libraries------------------------------------------------------------------
 library("sqldf")
 
-#Check for file, and DL if not available
+#Check for file, and DL if not available------------------------------------------
 fileURL <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
 destfile <- "StormData.csv"
 if(!file.exists(destfile))
@@ -10,10 +10,10 @@ if(!file.exists(destfile))
                 destfile)
   }
 
-#read main data into a dataframe
+#read main data into a dataframe-------------------------------------------------
 StormData <- read.csv("StormData.csv", header = TRUE, stringsAsFactors = FALSE)
 
-#Get data on the injuries and fatalities happening 
+#Get data on the injuries and fatalities  ---------------------------------------------
 GetIntialPplDMG <- sqldf::sqldf("
 SELECT STATE, 
 EVTYPE, 
@@ -29,24 +29,39 @@ PplDMG_GroupedBy_EVTYPE <-  sqldf::sqldf("SELECT EVTYPE, SUM(FatalAndInjuresComb
                                          GROUP BY EVTYPE
                                          ORDER BY SUM(FatalAndInjuresCombined) DESC")
 
-#PROPDMGEXP
+
+
+#Get data on economic effect ------------------------------------------------------
+
+#find unique values of PROPDMGEXP
 PROPDMGEXP_uniqueElements <- sqldf::sqldf("SELECT PROPDMGEXP, COUNT(PROPDMGEXP) 
                                           FROM StormData
                                           GROUP BY PROPDMGEXP")
 
+
+
 #find unique values of CROPDMGEXP
-CROPDMGEXP_uniqueElements <- sqldf::sqldf("SELECT CROPDMGEXP, COUNT(PROPDMGEXP) 
+CROPDMGEXP_uniqueElements <- sqldf::sqldf("SELECT CROPDMGEXP, COUNT(CROPDMGEXP) 
                                           FROM StormData
                                           GROUP BY CROPDMGEXP")
 
 
-#Get data on economic effect
-# GetIntialPplDMG <- sqldf::sqldf("
-# SELECT STATE, 
-# EVTYPE, 
-# , 
-# SUM(INJURIES) as TotalStateInjuries, 
-# (SUM(FATALITIES) + SUM(INJURIES)) AS FatalAndInjuresCombined
-# FROM StormData
-# GROUP BY STATE, EVTYPE
-# ORDER BY STATE, EVTYPE")
+#20180502 1325 At this time it would appear that the remarks
+#field will not help identify the unique value exp
+#Maybe explore the dropping of these?
+#query to figure out if the strange EPX values are explained in remarks
+CROP_PROPDMGEXP_StrangeValueRemarks <- sqldf::sqldf(
+"SELECT PROPDMG, 
+PROPDMGEXP,
+CROPDMG,
+CROPDMGEXP, 
+REMARKS
+FROM StormData
+WHERE REMARKS <> ''
+")
+#removed from above query as they had no appearent effect
+#AND (PROPDMGEXP <> 'K' OR PROPDMGEXP <> 'B' OR PROPDMGEXP <> '0' OR PROPDMGEXP <> '')
+#AND (CROPDMGEXP <> 'K' OR CROPDMGEXP <> 'B' OR CROPDMGEXP <> '0' OR CROPDMGEXP <> '')
+
+# The query for CROP and PROP will have to have cases that
+# account for the variation in the EXp values
