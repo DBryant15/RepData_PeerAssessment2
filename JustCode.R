434172e0cde3a7c6a172f7850c90c4038ae09dfa
Derek <DBryant15>
2018-05-05 10:18:41 -0500

    20180505 1014 DWB  - Added some queryies to explore the EXP columns and values associated.
    The next step should be:
    - review the output of EXP explore queries and ensure that the method from INFO is acceptable. At this point I believe that it is.
    - to decide how the values will be used to create a new column.

--- JustCode.R
+++ JustCode.R
@@ -33,32 +33,68 @@ PplDMG_GroupedBy_EVTYPE <-  sqldf::sqldf("SELECT EVTYPE, SUM(FatalAndInjuresComb
 
 #Get data on economic effect ------------------------------------------------------
 
-#find unique values of PROPDMGEXP
-PROPDMGEXP_uniqueElements <- sqldf::sqldf("SELECT PROPDMGEXP, COUNT(PROPDMGEXP) 
+##find unique values of PROPDMGEXP--------------------------
+PROPDMGEXP_COUNTuniqueElements <- sqldf::sqldf("SELECT PROPDMGEXP, COUNT(PROPDMGEXP) 
                                           FROM StormData
                                           GROUP BY PROPDMGEXP")
 
 
 
-#find unique values of CROPDMGEXP
-CROPDMGEXP_uniqueElements <- sqldf::sqldf("SELECT CROPDMGEXP, COUNT(CROPDMGEXP) 
-                                          FROM StormData
-                                          GROUP BY CROPDMGEXP")
+##find unique values of CROPDMGEXP--------------------------------
+CROPDMGEXP_COUNTuniqueElements <- sqldf::sqldf(
+"SELECT CROPDMGEXP, COUNT(CROPDMGEXP) 
+FROM StormData
+GROUP BY CROPDMGEXP")
 
+##Statements to explore exact values in EXP
 
+###Result of this QRY implys that 465934 (the majority of PROPDMGEXP) are =''----------
+PROPDMGEXP_EXPLOREuniqueElements <- sqldf::sqldf(
+"SELECT COUNT(PROPDMGEXP) 
+FROM StormData
+WHERE PROPDMGEXP = '' 
+GROUP BY PROPDMGEXP")
+
+###Result of this QRY: grouping the 465934 to see what they are ----------
+PROPDMGEXP_EXPLORE2uniqueElements <- sqldf::sqldf("
+SELECT PROPDMG, count(PROPDMG) 
+FROM StormData
+WHERE PROPDMGEXP = '' 
+GROUP BY PROPDMG")
+
+###Result of this QRY implys that 618413 (the majority of CROPDMGEXP) are =''---------
+CROPDMGEXP_EXPLOREuniqueElements <- sqldf::sqldf("
+SELECT COUNT(CROPDMGEXP) 
+FROM StormData 
+WHERE CROPDMGEXP = ''
+GROUP BY CROPDMGEXP")
+
+###Result of this QRY: grouping the 618413 to see what they are ----------
+CROPDMGEXP_EXPLORE2uniqueElements <- sqldf::sqldf("
+SELECT CROPDMG, count(CROPDMG)
+FROM StormData
+WHERE CROPDMGEXP = '' 
+GROUP BY CROPDMG")
+
+##TODO: this will be the case statement to handle the unique values of StormData-----
+
+StormData_w_ColumnforFullNumericCost <- sqldf::sqldf("SELECT *, '1' as new_Column
+                                                     FROM StormData")
+
+##Exploratory statement: Get non-blank REMARKS------------------------------------
 #20180502 1325 At this time it would appear that the remarks
 #field will not help identify the unique value exp
 #Maybe explore the dropping of these?
-#query to figure out if the strange EPX values are explained in remarks
-CROP_PROPDMGEXP_StrangeValueRemarks <- sqldf::sqldf(
-"SELECT PROPDMG, 
-PROPDMGEXP,
-CROPDMG,
-CROPDMGEXP, 
-REMARKS
-FROM StormData
-WHERE REMARKS <> ''
-")
+#query to figure out if the strange EXP values are explained in remarks
+# CROP_PROPDMGEXP_StrangeValueRemarks <- sqldf::sqldf(
+# "SELECT PROPDMG, 
+# PROPDMGEXP,
+# CROPDMG,
+# CROPDMGEXP, 
+# REMARKS
+# FROM StormData
+# WHERE REMARKS <> ''
+# ")
 #removed from above query as they had no appearent effect
 #AND (PROPDMGEXP <> 'K' OR PROPDMGEXP <> 'B' OR PROPDMGEXP <> '0' OR PROPDMGEXP <> '')
 #AND (CROPDMGEXP <> 'K' OR CROPDMGEXP <> 'B' OR CROPDMGEXP <> '0' OR CROPDMGEXP <> '')
